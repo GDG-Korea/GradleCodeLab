c2c305d0488be558c2546ba9f22aff75e607445d
hl5pma <hl5pma@gmail.com>
2014-05-03 03:21:29 +0900

    고흐 flavor 파일들 추가

---
+++ PicassoProject/gogh_flavor_files/Data.java
@@ -0,0 +1,26 @@
+package com.example.picasso;
+
+final class Data {
+  static final String[] URLS = {
+      "http://www.vangoghgallery.com/catalog/image/0612/Starry-Night.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0467/Caf%C3%A9-Terrace-on-the-Place-du-Forum,-Arles,-at-Night,-The.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0457/Still-Life:-Vase-with-Fifteen-Sunflowers.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0413/Fishing-Boats-on-the-Beach-at-Saintes-Maries.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0295/Self-Portrait-with-Grey-Felt-Hat.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0527/Self-Portrait-with-Bandaged-Ear.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0301/Bridge-across-the-Seine-at-Asnieres.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0025/Congregation-Leaving-the-Reformed-Church-in-Nuenen.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0675/Cottages-and-Cypresses:-Reminiscence-of-the-North.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0519/Courtyard-of-the-Hospital-at-Arles,-The.jpgv",
+      "http://www.vangoghgallery.com/catalog/image/0621/Cypresses-and-Two-Women.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0445/Encampment-of-Gypsies-with-Caravans.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0641/Enclosed-Wheat-Field-with-Peasant.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0318/Factory-at-Asnieres,-The.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0720/Field-of-Spring-Wheat-at-Sunrise.jpg",
+      "http://www.vangoghgallery.com/catalog/image/0706/Field-with-Ploughman-and-Mill.jpg"
+  };
+
+  private Data() {
+    // No instances.
+  }
+}
